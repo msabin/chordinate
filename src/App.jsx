989afbe375c89ebcc20790f441fe6c178374f0ed
Manuel Sabin <msabin27@gmail.com>
2024-01-15 22:45:29 -0800

    Added support for computer keyboard to be played like a piano as well

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,36 @@ function App() {
 
   useEffect(() => {
     midiSetup();
+
+    const keyboard = ['a', 'w', 's', 'e', 'd', 'f', 
+        't', 'g', 'y', 'h', 'u', 'j', 'k', 'o', 'l', 
+        'p', ';']
+
+    function handleKeyDown(e) {
+
+      const midiNote = keyboard.indexOf(e.key.toLowerCase()) + 60
+      if (midiNote !== 59) {
+        const key = document.getElementById(midiNote)
+        key.ariaPressed = 'true';
+      }
+    }
+
+    function handleKeyUp(e) {
+
+      const midiNote = keyboard.indexOf(e.key.toLowerCase()) + 60
+      if (midiNote !== 59) {
+        const key = document.getElementById(midiNote)
+        key.ariaPressed = 'false';
+      }
+    }
+
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keydown', handleKeyUp);
+    }
   }, [])
 
   return (
