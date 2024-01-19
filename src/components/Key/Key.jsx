198032e5cf2962bc76df41f2d772f9802cd354a6
Manuel Sabin <msabin27@gmail.com>
2024-01-18 17:43:15 -0800

    Black keys are almost same width as tops of white keys now.  Should make it easy to put letters/numbers above the keys uniformly now.  Had to scale in the x-axis of the black keys a bit more than the y-axis.  Made variables to adjust all the parameters easier and make it more readable. Nothing is exact right now, but it's starting to look pretty decent.

--- src/components/Key/Key.jsx
+++ src/components/Key/Key.jsx
@@ -3,12 +3,27 @@ import styles from "./index.module.css";
 export function Key(props) {
   const { midiNote } = props;
 
-  const isBlack = [1, 3, 6, 8, 10].includes(midiNote % 12);
+  const scaleNum = midiNote % 12;
+
+  const isBlack = [1, 3, 6, 8, 10].includes(scaleNum);
+
+  let isLeft, isRight;
+  if ( scaleNum === 1 || scaleNum === 6 ) {
+    isLeft = true;
+  }
+  else if ( scaleNum === 3 || scaleNum === 10) {
+    isRight = true
+  }
 
   return (
     <div 
       id={midiNote}
-      className = {`${styles.key} ${isBlack ? styles.black : null}`}
+      className = {`
+        ${styles.key} 
+        ${isBlack ? styles.black : null}
+        ${isLeft ? styles.left : null}
+        ${isRight ? styles.right : null}
+        `}
       ></div>
   )
 }
