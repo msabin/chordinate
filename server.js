05fbe8c5d72b91f3adfafe4ca4afa16dff3cad3e
Manuel Sabin <msabin27@gmail.com>
2024-01-25 23:27:25 -0800

    Removed the github pages build, added some package.json stuff for deployment, and it's now deployed and working on Glitch for now!

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const io = new Server(server);
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use('/chordinate', express.static(join(__dirname, 'dist')));
+app.use(express.static(join(__dirname, 'dist')));
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'dist/index.html'));
